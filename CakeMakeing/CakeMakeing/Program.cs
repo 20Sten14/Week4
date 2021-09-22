dc6ea7119be252599d22c2f92f53efd431f5dea1
Opilane TTHK <opilane@tthk.ee>
2021-09-22 11:50:05 +0300

    Switch

---
+++ CakeMakeing/CakeMakeing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CakeMakeing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cookies wide is the cake?");
+            int CWide = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many cookies in lenght is the cake?");
+            int CLong = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many layers does the cake have?");
+            int CLayer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many cookies in a pack of cookies?");
+            int Cpack = Convert.ToInt32(Console.ReadLine());
+            int CArea = CLong * CWide;
+            int CALL = CLayer * CArea;
+            int CPALL = Convert.ToInt32(Math.Round((double)CALL / Cpack));
+                Console.WriteLine($"you need {CPALL} packs of cookies");
+        }
+    }
+    
+}
